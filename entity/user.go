91caae5d93d82a89ca362eda25550db7b4e63a88
Akinori ABE <abe_akinori@cyberagent.co.jp>
2016-04-11 19:56:13 +0900

    Add UserDetail entity

--- entity/user.go
+++ entity/user.go
@@ -6,17 +6,29 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-// User ... structure of a user
-type User struct {
-	UserID                    bson.ObjectId `json:"userId"                              bson:"userId"                              validate:"objectId"`
-	Name                      string        `json:"name"                                bson:"name"                                validate:"min=1"`
-	ScreenName                string        `json:"screenName"                          bson:"screenName"                          validate:"min=1"`
-	ProfileImageURL           string        `json:"profileImageUrl,omitempty"           bson:"profileImageUrl,omitempty"`
-	ProfileBackgroundImageURL string        `json:"profileBackgroundImageUrl,omitempty" bson:"profileBackgroundImageUrl,omitempty"`
-	Biography                 string        `json:"biography,omitempty"                 bson:"biography,omitempty"`
-	LocationText              string        `json:"locationText,omitempty"              bson:"localtionText,omitempty"`
-	URL                       string        `json:"url,omitempty"                       bson:"url,omitempty"`
-	Birthday                  *time.Time    `json:"time,omitempty"                      bson:"time,omitempty"`
-	CreatedAt                 time.Time     `json:"createdAt"                           bson:"createdAt"`
-	UpdatedAt                 time.Time     `json:"updatedAt"                           bson:"updatedAt"`
-}
+type (
+	// User ... structure of a user
+	User struct {
+		UserID                    bson.ObjectId `json:"userId"                              bson:"userId"                              validate:"objectId"`
+		Name                      string        `json:"name"                                bson:"name"                                validate:"min=1"`
+		ScreenName                string        `json:"screenName"                          bson:"screenName"                          validate:"min=1"`
+		ProfileImageURL           string        `json:"profileImageUrl,omitempty"           bson:"profileImageUrl,omitempty"`
+		ProfileBackgroundImageURL string        `json:"profileBackgroundImageUrl,omitempty" bson:"profileBackgroundImageUrl,omitempty"`
+		Biography                 string        `json:"biography,omitempty"                 bson:"biography,omitempty"`
+		LocationText              string        `json:"locationText,omitempty"              bson:"localtionText,omitempty"`
+		URL                       string        `json:"url,omitempty"                       bson:"url,omitempty"`
+		Birthday                  *time.Time    `json:"time,omitempty"                      bson:"time,omitempty"`
+		CreatedAt                 time.Time     `json:"createdAt"                           bson:"createdAt"`
+		UpdatedAt                 time.Time     `json:"updatedAt"                           bson:"updatedAt"`
+	}
+
+	// UserDetail ... structure of a user "more" information
+	UserDetail struct {
+		*User
+		TweetsCount    int   `json:"tweetsCount"`
+		LikesCount     int   `json:"likesCount"`
+		FollowerCount  int   `json:"followerCount"`
+		FollowingCount int   `json:"followingCount"`
+		Following      *bool `json:"following,omitempty"`
+	}
+)
