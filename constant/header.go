b6e817f8f37810e50010eabe741032d7e313be35
Akinori ABE <abe_akinori@cyberagent.co.jp>
2016-04-12 18:08:25 +0900

    Reflect review comments and cosmetic changes

--- constant/header.go
+++ constant/header.go
@@ -2,5 +2,5 @@ package constant
 
 const (
 	// XPoppoSessionID is a header name of user session id
-	XPoppoSessionID = "X-Beaut-Session-Id"
+	XPoppoSessionID = "X-Poppo-Session-Id"
 )
--- v1/users.go
+++ v1/users.go
@@ -1,10 +1,7 @@
 package v1
 
 import (
-	"fmt"
-
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/techcampman/twitter-d-server/constant"
@@ -15,41 +12,10 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-func iso8601(s string) (t time.Time) {
-	t, err := time.Parse(time.RFC3339, s)
-	if err != nil {
-		fmt.Println("Oops!")
-	}
-	return
-}
-
 func getUser(c *gin.Context) {
 	id := utils.GetObjectIDPath(c, constant.IDKey)
 
-	users, err := collection.Users()
-	if err != nil {
-		errors.Send(c, err)
-		return
-	}
-	defer users.Close()
-
-	// Test Data
-	/*users.Insert(&entity.User{
-		UserID: bson.NewObjectId(), // 570c7bcd302125232a3021ca
-		Name: "ほげふが",
-		ScreenName: "hogehuga",
-		ProfileImageURL: "https://pbs.twimg.com/profile_images/666407537084796928/YBGgi9BO.png",
-		ProfileBackgroundImageURL: "https://pbs.twimg.com/profile_banners/2931934340/1456564386",
-		Biography: "よろしくおねがいします。",
-		LocationText: "日本, 渋谷, 道玄坂",
-		URL: "https://www.cyberagent.co.jp/",
-		Birthday: nil,
-		CreatedAt: iso8601("2016-03-20T12:34:56+09:00"),
-		UpdatedAt: iso8601("2016-04-11T12:34:56+09:00"),
-	})*/
-
-	u := new(entity.User)
-	err = users.Find(bson.M{"_id": id}).One(u)
+	u, err := getUserInfo(id)
 	if err != nil {
 		errors.Send(c, err)
 		return
@@ -70,6 +36,18 @@ func getUser(c *gin.Context) {
 	ud := &entity.UserDetail{u, tweetsCount, likesCount, followerCount, followingCount, nil}
 
 	c.JSON(http.StatusOK, ud)
+}
+
+func getUserInfo(id bson.ObjectId) (u *entity.User, err error) {
+	users, err := collection.Users()
+	if err != nil {
+		return
+	}
+	defer users.Close()
+
+	u = new(entity.User)
+	err = users.Find(bson.M{"_id": id}).One(u)
+	return
 }
 
 func getTweetsInfo(id bson.ObjectId) (tweetsCount int, likesCount int, err error) {
@@ -101,8 +79,5 @@ func getFollowsInfo(id bson.ObjectId) (followingCount int, followerCount int, er
 	}
 
 	followerCount, err = follows.Find(bson.M{"targetId": id}).Count()
-	if err != nil {
-		return
-	}
 	return
 }
