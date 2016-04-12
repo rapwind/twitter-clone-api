234854e4faf659e50b99a49bdeba30b804bba3e5
Akinori ABE <abe_akinori@cyberagent.co.jp>
2016-04-12 10:23:46 +0900

    Add validate annotation to fields in Follow entity

--- entity/follow.go
+++ entity/follow.go
@@ -5,8 +5,8 @@ import "gopkg.in/mgo.v2/bson"
 type (
 	// Follow ... structure of a follow
 	Follow struct {
-		ID       bson.ObjectId `json:"-"        bson:"_id"`
-		UserID   bson.ObjectId `json:"userId"   bson:"userId"`
-		TargetID bson.ObjectId `json:"targetId" bson:"targetId"`
+		ID       bson.ObjectId `json:"-"        bson:"_id"      validate:"objectId"`
+		UserID   bson.ObjectId `json:"userId"   bson:"userId"   validate:"objectId"`
+		TargetID bson.ObjectId `json:"targetId" bson:"targetId" validate:"objectId"`
 	}
 )
