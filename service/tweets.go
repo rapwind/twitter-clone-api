edfc8fcf02a2788374e8187074f21e0dd5d989ea
Akinori ABE <abe_akinori@cyberagent.co.jp>
2016-04-13 11:12:34 +0900

    Change ReadTweetsCountsByUserID |-> ReadTweetsCountsByUser

--- service/tweets.go
+++ service/tweets.go
@@ -2,18 +2,19 @@ package service
 
 import (
 	"github.com/techcampman/twitter-d-server/db/collection"
+	"github.com/techcampman/twitter-d-server/entity"
 	"gopkg.in/mgo.v2/bson"
 )
 
-// ReadTweetsCountsByUserID gets entity.UserDetail.TweetsCount and LikesCount
-func ReadTweetsCountsByUserID(id bson.ObjectId) (tweetsCount int, likesCount int, err error) {
+// ReadTweetsCountsByUser gets entity.UserDetail.TweetsCount and LikesCount
+func ReadTweetsCountsByUser(u entity.User) (tweetsCount int, likesCount int, err error) {
 	tweets, err := collection.Tweets()
 	if err != nil {
 		return
 	}
 	defer tweets.Close()
 
-	tweetsCount, err = tweets.Find(bson.M{"userId": id}).Count()
+	tweetsCount, err = tweets.Find(bson.M{"userId": u.UserID}).Count()
 	if err != nil {
 		return
 	}
--- v1/users.go
+++ v1/users.go
@@ -20,7 +20,7 @@ func getUser(c *gin.Context) {
 		return
 	}
 
-	tweetsCount, likesCount, err := service.ReadTweetsCountsByUserID(id)
+	tweetsCount, likesCount, err := service.ReadTweetsCountsByUser(*u)
 	if err != nil {
 		errors.Send(c, err)
 		return
