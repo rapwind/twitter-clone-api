56f94013326c0392916f68422fccd392cbb11e9f
Akinori ABE <abe_akinori@cyberagent.co.jp>
2016-04-25 15:24:59 +0900

    Add constant/image.go (I've forgot)

---
+++ constant/image.go
@@ -0,0 +1,12 @@
+package constant
+
+const (
+	// ImageTypeProfile ... profile images
+	ImageTypeProfile = "profile"
+
+	// ImageTypeHeader ... header images
+	ImageTypeHeader = "header"
+
+	// ImageTypeTweet ... tweet images
+	ImageTypeTweet = "tweet"
+)
